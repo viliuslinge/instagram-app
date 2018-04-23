09d6a2a648e9fd04f92d26f40d126ded5d7d6cda
Vilius Linge <viliusling@gmail.com>
2018-04-23 09:10:50 +0300

    App with firebase init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    apiKey: 'AIzaSyAqAJZ3yMXtv3wVxajoULc2I_PbRD3nrDo',
+    authDomain: 'instagram-app-6a413.firebaseapp.com',
+    databaseURL: 'https://instagram-app-6a413.firebaseio.com',
+    projectId: 'instagram-app-6a413',
+    storageBucket: 'instagram-app-6a413.appspot.com',
+    messagingSenderId: '135542056880'
+  }
 };
